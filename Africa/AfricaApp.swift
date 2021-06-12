2fe6e313ae7f6c0ea3cf6cea3a1cbbb50784a5f1
Brian Lee <brian.ts.lee.0907@gmail.com>
2021-06-12 22:58:36 +0900

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Brian Lee on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
